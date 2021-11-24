08593066520bcc90873cfeb506abf1d1b9f6a912
Kim Min-Cheol <sksqwer123@gmail.com>
2021-11-24 09:55:30 +0900

    1

--- Framework/Meshes/MeshCube.cpp
+++ Framework/Meshes/MeshCube.cpp
@@ -30,7 +30,7 @@ void MeshCube::Create()
 	v.push_back(MeshVertex(-w, -h, +d, 0, 0, -1));
 	v.push_back(MeshVertex(+w, -h, +d, 0, 0, -1));
 	v.push_back(MeshVertex(+w, +h, +d, 0, 0, -1));
-	v.push_back(MeshVertex(-w, -h, +d, 0, 0, -1));
+	v.push_back(MeshVertex(-w, +h, +d, 0, 0, -1));
 
 	// : top
 	v.push_back(MeshVertex(-w, +h, -d, 0, 0, -1));
@@ -64,12 +64,12 @@ void MeshCube::Create()
 	indexCount = 36;
 	this->indices = new UINT[indexCount]
 	{
-		0,1,2, 2,1,3,
-		4,5,6, 4,6,7,
-		8,9,10, 8,10,11,
-		12,13,14, 12,14,15,
-		16,17,18, 16,18,19,
-		20,21,22, 20,22,23
+	 0, 1, 2, 0, 2, 3,
+	  4, 5, 6, 4, 6, 7,
+	  8, 9, 10, 8, 10, 11,
+	  12, 13, 14, 12, 14, 15,
+	  16, 17, 18, 16, 18, 19,
+	  20, 21, 22, 20, 22, 23
 	};
 
 }
